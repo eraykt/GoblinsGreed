76bdca73d02fa517bfeb86cecdb42b30d28aca98
eray <alieraykt@gmail.com>
2025-10-26 23:52:37 +0300

    Add blend tree and update player animations
    Introduced a blend tree to the PlayerAnimationController for smoother transitions between idle and movement states. Updated several animation .fbx and .meta files to reference the correct asset origin and improved animation import settings for movement animations.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -2,7 +2,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private static readonly int Move = Animator.StringToHash("Move");
+    
     private Mover mover;
+    private Animator animator;
+    
     
     [SerializeField]
     private Joystick joystick;
@@ -10,12 +14,14 @@ public class PlayerController : MonoBehaviour
     private void Awake()
     {
         mover = GetComponent<Mover>();
+        animator = GetComponent<Animator>();
         mover.AlignToGround(transform);
     }
     
     private void Update()
     {
         mover.SetMoveVector(joystick.Direction);
+        animator.SetFloat(Move, joystick.Direction.magnitude);
     }
 
     private void FixedUpdate()
